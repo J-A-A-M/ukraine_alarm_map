67378909712b614dd04926707caf395b246df05b
v00g100skr <utmcrow@gmail.com>
2023-06-03 03:21:25 +0300

    map editor improvements: brightness, blink count, back to initial mode, color modulation, increase weather polling, map editor improvements: design, added almost all config values to editor

--- firmware.h
+++ firmware.h
@@ -3,22 +3,38 @@
 //Налаштування WiFi
 char* ssid = ""; //Назва твоєї мережі WiFi
 char* password = ""; //Пароль від твого WiFi
+char* APSsid = "AlarmMap"; //Назва точки доступу щоб переналаштувати WiFi
+char* APPassword = ""; //Пароль від точки доступу щоб переналаштувати WiFi. Пусте - без пароля, рекомендую так і залишити (пароль від 8 симолів)
 
 //Налштування за замовчуванням
 bool enabled = true; //Ввімкнена/вимкнена карта
 int brightness = 100; //Яскравість %
-bool autoBrightness = true; //Ввімкнена/вимкнена авто яскравість
-int mode = 1; //Режим
+int alarm_brightness[] = {
+  0,
+  100,
+  20,
+  50
+};
+bool autoBrightness = false; //Ввімкнена/вимкнена авто яскравість
 bool autoSwitch = true; //Автоматичне переключення карти на режим тривоги при початку тривоги в вибраній області, після заверешення тривоги в вибраній області режим не повертається на своє місце
 static bool greenStates = true; //true - області без тривоги будуть зелені; false - не будуть світитися
+
+int mapModeInit = 1;
+int mapMode = 1; //Режим
+
 bool blink = true;
+int blinkCount = 2;
 int blinkDistricts[] = {
-  7
+  7,
+  8
 };
 
-//Налаштування телеграм бота
-#define BOTtoken ""
-#define CHAT_ID ""
+int modulationMode = 1;
+int modulationStep = 10;
+int modulationTime = 100;
+int modulationCount = 2;
+
+int hv = 60000;
 
 //Налаштування авто-яскравості
 const int day = 8; //Початок дня
@@ -40,7 +56,7 @@ int statesIdsCheck[] = { //Вибери області при тривозі в 
 0, //Волинська область
 0, //Рівненська область
 0, //Житомирська область
-0, //Київ
+1, //Київ
 0, //Київська область
 0, //Чернігівська область
 0, //Сумська область
@@ -145,28 +161,28 @@ WebServer server(80);
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 DynamicJsonDocument doc(30000);
-String baseURL = "https://vadimklimenko.com/map/statuses.json";
+//String baseURL = "https://vadimklimenko.com/map/statuses.json";
+String baseURL = "https://map.vglskr.net.ua/alarm_map";
 WiFiClientSecure client;
 WiFiManager wm;
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "ua.pool.ntp.org", 7200);
-UniversalTelegramBot bot(BOTtoken, client);
-unsigned long lastTimeBotRan;
 static unsigned long times[27];
 static int ledColor[27];
-static int ledColorBlue[] = { 4,5,6,7,8,9,10,11,12,21,22, };
+static int ledColorBlue[] = { 4,5,6,7,8,9,10,11,12,21,22 };
 static int ledColorYellow[] = { 0,1,2,3,12,13,14,15,16,17,18,19,20,23,24,25,26 };
+int arrDistrictsSize = sizeof(blinkDistricts) / sizeof(int);
 int arrAlarms = sizeof(ledColor) / sizeof(int);
 int arrSize = sizeof(states) / sizeof(String);
-int arrDistrictsSize = sizeof(blinkDistricts) / sizeof(int);
 bool enable = false;
-int period = 10000;
-int weather_period = 300000;
+int alarms_period = 15000;
+int weather_period = 600000;
+unsigned long lastAlarmsTime;
 unsigned long lastWeatherTime;
+static bool firstAlarmsUpdate = true;
 static bool firstWeatherUpdate = true;
 int alarmsNowCount = 0;
 static bool wifiConnected;
-static bool firstUpdate = true;
 
 
 void setup_routing() {	 	 ;
@@ -186,16 +202,17 @@ void handlePost() {
 
   // Get brightness
   int brightness = jsonDocument["brightness"];
-  int auto_brightness_on = jsonDocument["auto_brightness_on"];
+  int auto_brightness = jsonDocument["auto_brightness"];
+  int map_mode = jsonDocument["map_mode"];
   int green_states_on = jsonDocument["green_states_on"];
   int green_states_off = jsonDocument["green_states_off"];
   int map_enable = jsonDocument["map_enable"];
   int map_disable = jsonDocument["map_disable"];
-  int set_mode = jsonDocument["mode"];
+
   int blink_enable = jsonDocument["blink_enable"];
   int blink_disable = jsonDocument["blink_disable"];
-  Serial.print("brightness: ");
-  Serial.print(brightness);
+  int modulation_mode = jsonDocument["modulation_mode"];
+  int set_hv = jsonDocument["hv"];
 
   if(brightness) {
     autoBrightness = false;
@@ -203,7 +220,7 @@ void handlePost() {
     strip.show();
   }
 
-  if(auto_brightness_on) {
+  if(auto_brightness) {
     autoBrightness = true;
   }
 
@@ -215,8 +232,8 @@ void handlePost() {
     greenStates = false;
   }
 
-  if(set_mode) {
-    mode = set_mode;
+  if(map_mode) {
+    mapModeInit = map_mode;
   }
 
   if(map_enable) {
@@ -235,6 +252,14 @@ void handlePost() {
     blink = false;
   }
 
+  if(modulation_mode) {
+    modulationMode = modulation_mode;
+  }
+
+  if (set_hv) {
+    hv = set_hv*1000;
+  }
+
   // Respond to the client
   server.send(200, "application/json", "{}");
 }
@@ -244,14 +269,16 @@ void getEnv() {
   jsonDocument.clear();
   jsonDocument["brightness"] = brightness;
   jsonDocument["autobrightness"] = autoBrightness;
-  jsonDocument["mode"] = mode;
+  jsonDocument["mapMode"] = mapMode;
+  jsonDocument["mapModeInit"] = mapModeInit;
   jsonDocument["autoSwitch"] = autoSwitch;
   jsonDocument["enabled"] = enabled;
   jsonDocument["greenStates"] = greenStates;
   jsonDocument["alarmsNowCount"] = alarmsNowCount;
   jsonDocument["greenStates"] = greenStates;
   jsonDocument["blink"] = blink;
-  jsonDocument["period"] = period;
+  jsonDocument["modulationMode"] = modulationMode;
+  jsonDocument["hv"] = hv;
   serializeJson(jsonDocument, buffer);
   server.send(200, "application/json", buffer);
 }
@@ -272,11 +299,88 @@ void initWiFi() {
     }
   }
 }
-void colorWipe(int wait) {
+void Modulation(int count) {
+  int pixel_brightness_start;
+  int pixel_brightness_end;
+  int pixel_color_full;
+  int pixel_color_penta;
+  if (alarm_brightness[1] >= alarm_brightness[2]) {
+    pixel_brightness_start = static_cast<int>(alarm_brightness[2]*brightness/100);
+    pixel_brightness_end = static_cast<int>(alarm_brightness[1]*brightness/100);
+  } else {
+    pixel_brightness_start = static_cast<int>(alarm_brightness[1]*brightness/100);
+    pixel_brightness_end = static_cast<int>(alarm_brightness[2]*brightness/100);
+  }
+  for (int i = 0; i < count; i++) {
+    for (int i = pixel_brightness_end; i >= pixel_brightness_start; i -= modulationStep) {
+      pixel_color_full= static_cast<int>(255*i/100);
+      pixel_color_penta = static_cast<int>(55*i/100);
+      for (int i = 0; i < arrSize; i++)
+      {
+        switch (ledColor[i]) {
+          case 1: strip.setPixelColor(i, strip.Color(255, 0, 0)); break;
+          case 2: strip.setPixelColor(i, strip.Color(pixel_color_full, pixel_color_penta, 0)); break;
+          case 0: if (greenStates) {} else {strip.setPixelColor(i, strip.Color(0, 0, 0)); break;}
+          case 3: strip.setPixelColor(i, strip.Color(0, 255, 0)); break;
+        }
+      }
+      strip.show();
+      delay(modulationTime);
+    }
+    for (int i = pixel_brightness_start; i <= pixel_brightness_end; i += modulationStep) {
+      pixel_color_full= static_cast<int>(255*i/100);
+      pixel_color_penta = static_cast<int>(55*i/100);
+      for (int i = 0; i < arrSize; i++)
+      {
+        switch (ledColor[i]) {
+          case 1: strip.setPixelColor(i, strip.Color(255, 0, 0)); break;
+          case 2: strip.setPixelColor(i, strip.Color(pixel_color_full, pixel_color_penta, 0));; break;
+          case 0: if (greenStates) {} else {strip.setPixelColor(i, strip.Color(0, 0, 0)); break;}
+          case 3: strip.setPixelColor(i, strip.Color(0, 255, 0)); break;
+        }
+      }
+      strip.show();
+      delay(modulationTime);
+    }
+  }
+}
+void Blink(int count) {
+  //BLYNK
+  int blinkCounter = count*2+1;
+  bool blinkState = false;
+
+  //if (ledColor[1] == 1 || ledColor[1] == 2) { // Якщо 1 лампочка світить червоним або жовтим кольором
+    for (int i = 0; i < blinkCounter; i++) {
+      blinkState = !blinkState;
+      for (int i = 0; i < arrDistrictsSize; i++) {
+        if (blinkState) {
+          switch (ledColor[blinkDistricts[i]]) {
+          case 1: strip.setPixelColor(blinkDistricts[i], strip.Color(255, 0, 0)); break;
+          case 2: strip.setPixelColor(blinkDistricts[i], strip.Color(255, 55, 0)); break;
+          case 0: if (greenStates) {} else {strip.setPixelColor(blinkDistricts[i], strip.Color(0, 0, 0)); break;}
+          case 3: strip.setPixelColor(blinkDistricts[i], strip.Color(0, 255, 0)); break;
+          }
+        } else {
+          strip.setPixelColor(blinkDistricts[i], strip.Color(0, 0, 0)); // Вимкнути 1 лампочку
+        }
+        strip.show();
+      } // Оновити світлодіодну стрічку
+      delay(1000); // Затримка 1 секунда
+    }
+  //}
+  //BLYNK
+}
+void Flag(int wait) {
+  //strip.setPixelColor(ledColorBlue[i], strip.Color(0,191,255));
+  //strip.setPixelColor(ledColorYellow[i], strip.Color(255,255,51));
   int count = sizeof(ledColorYellow) / sizeof(int);
-  for (int i = 0; i < count; i++) { // For each pixel in strip...
-    strip.setPixelColor(ledColorBlue[i], strip.Color(0,191,255));
-    strip.setPixelColor(ledColorYellow[i], strip.Color(255,255,51));//  Set pixel's color (in RAM)
+  for (int i = 0; i < 11; i++) { // For each pixel in strip...
+    strip.setPixelColor(ledColorBlue[i], strip.Color(0,255,255));
+    strip.show();                          //  Update strip to match
+    delay(wait);                           //  Pause for a moment
+  }
+  for (int i = 0; i < 17; i++) { // For each pixel in strip...
+    strip.setPixelColor(ledColorYellow[i], strip.Color(255,255,0));
     strip.show();                          //  Update strip to match
     delay(wait);                           //  Pause for a moment
   }
@@ -285,7 +389,7 @@ void initStrip() {
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.show();            // Turn OFF all pixels ASAP
   strip.setBrightness(brightness * 2.55);
-  colorWipe(120);
+  Flag(120);
 }
 void initTime() {
   // Встановлюємо початкове значення літнього часу на false
@@ -327,7 +431,7 @@ void loop() {
 	wifiConnected = WiFi.status() == WL_CONNECTED;
 
   if (!wifiConnected) {
-    colorWipe(10);
+    Flag(10);
     delay(10000);
     ESP.restart();
   }
@@ -345,60 +449,70 @@ void loop() {
         }
         strip.show();
       }
-      firstUpdate = false;
-      String response;
-      HTTPClient http;
-      http.begin(baseURL.c_str());
-      // Send HTTP GET request
-      int httpResponseCode = http.GET();
 
-      if (httpResponseCode == 200) {
-        response = http.getString();
-      }
-      else {
-        return;
-      }
-      // Free resources
-      http.end();
-      DeserializationError error = deserializeJson(doc, response);
-      if (error) {
-        return;
-      }
-      unsigned long  t = millis();
-      unsigned long hv = 180000;
-      alarmsNowCount = 0;
-      for (int i = 0; i < arrSize; i++) {
-        enable = doc["states"][states[i]]["enabled"].as<bool>();
-        if (enable && times[i] == 0) {
-          times[i] = t;
-          ledColor[i] = 2;
-          alarmsNowCount++;
-        }
-        else if (enable && times[i] + hv > t && ledColor[i] != 1) {
-          ledColor[i] = 2;
-          alarmsNowCount++;
+      //тривоги
+      if (millis() - lastAlarmsTime > alarms_period || firstAlarmsUpdate) {
+        firstAlarmsUpdate = false;
+        String response;
+        HTTPClient http;
+        http.begin(baseURL.c_str());
+        // Send HTTP GET request
+        int httpResponseCode = http.GET();
 
+        if (httpResponseCode == 200) {
+          response = http.getString();
         }
-        else if (enable) {
-          ledColor[i] = 1;
-          times[i] = t;
-          alarmsNowCount++;
+        else {
+          return;
+        }
+        // Free resources
+        http.end();
+        DeserializationError error = deserializeJson(doc, response);
+        if (error) {
+          return;
         }
 
-        if (!enable && times[i] + hv > t && times[i] != 0) {
-          ledColor[i] = 3;
-        }
-        else if (!enable) {
-          ledColor[i] = 0;
-          times[i] = 0;
-        }
+        unsigned long  t = millis();
+        alarmsNowCount = 0;
+        bool return_to_init_mode = true;
+        for (int i = 0; i < arrSize; i++) {
+          enable = doc["states"][states[i]]["enabled"].as<bool>();
+          if (enable && times[i] == 0) {
+            times[i] = t;
+            ledColor[i] = 2;
+            alarmsNowCount++;
+          }
+          else if (enable && times[i] + hv > t && ledColor[i] != 1) {
+            ledColor[i] = 2;
+            alarmsNowCount++;
 
-        if (autoSwitch && enable && statesIdsCheck[i]==1&& mode != 1) {
-            mode = 1;
+          }
+          else if (enable) {
+            ledColor[i] = 1;
+            times[i] = t;
+            alarmsNowCount++;
+          }
+
+          if (!enable && times[i] + hv > t && times[i] != 0) {
+            ledColor[i] = 3;
+          }
+          else if (!enable) {
+            ledColor[i] = 0;
+            times[i] = 0;
+          }
+
+          if (autoSwitch && enable && statesIdsCheck[i]==1) {
+              mapMode = 1;
+              return_to_init_mode = false;
+          }
+
+          if (return_to_init_mode) {
+            mapMode = mapModeInit;
+          }
         }
       }
-      if (mode == 1) {
-        //тривоги
+
+      if (mapMode == 1) {
         for (int i = 0; i < arrSize; i++)
         {
           switch (ledColor[i]) {
@@ -409,37 +523,14 @@ void loop() {
           }
         }
         strip.show();
+        if (modulationMode > 1) {
+          Modulation(modulationCount);
+        }
         if (blink) {
-          //BLYNK
-          int blinkCounter = 0;
-          bool blinkState = false;
-
-          //if (ledColor[1] == 1 || ledColor[1] == 2) { // Якщо 1 лампочка світить червоним або жовтим кольором
-            for (int i = 0; i < 20; i++) {
-              blinkCounter++;
-              blinkState = !blinkState;
-              for (int i = 0; i < arrDistrictsSize; i++) {
-                if (blinkState) {
-                  switch (ledColor[blinkDistricts[i]]) {
-                  case 1: strip.setPixelColor(blinkDistricts[i], strip.Color(255, 0, 0)); break;
-                  case 2: strip.setPixelColor(blinkDistricts[i], strip.Color(255, 55, 0)); break;
-                  case 0: if (greenStates) {} else {strip.setPixelColor(blinkDistricts[i], strip.Color(0, 0, 0)); break;}
-                  case 3: strip.setPixelColor(blinkDistricts[i], strip.Color(0, 255, 0)); break;
-                  }
-                } else {
-                  strip.setPixelColor(blinkDistricts[i], strip.Color(0, 0, 0)); // Вимкнути 1 лампочку
-                }
-                strip.show();
-              } // Оновити світлодіодну стрічку
-              delay(1000); // Затримка 1 секунда
-            }
-          //}
-          //BLYNK
-        } else {
-          delay(period);
+          Blink(blinkCount);
         }
       }
-      if (mode == 2) {
+      if (mapMode == 2) {
         if (millis() - lastWeatherTime > weather_period || firstWeatherUpdate) {
           // Loop through the city IDs and get the current weather for each city
           firstWeatherUpdate = false;
@@ -494,18 +585,16 @@ void loop() {
             strip.show();
           }
           lastWeatherTime = millis();
-          delay(period);
         }
       }
-      if (mode == 3) {
-        colorWipe(10);
-        delay(period);
+      if (mapMode == 3) {
+        Flag(10);
       }
     }
     else {
       strip.clear();
       strip.show();
-      delay(1000);
     }
+    delay(1000);
   }
 }
