08cea850f3073c634080948988048c96b8c9d25c
v00g100skr <utmcrow@gmail.com>
2023-06-06 21:48:41 +0300

    рефактор репозиторія, доданий viewer для даних мапи як окремий контейнер

---
+++ deploy/map_data/viewer.py
@@ -0,0 +1,36 @@
+from bottle import route, run, response
+
+import logging, os
+
+from pymemcache.client import base
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        #logging.FileHandler("debug.log"),
+        logging.StreamHandler()
+    ]
+)
+
+host = os.environ.get('MEMCHACHED_HOST') or 'memcached'
+port = os.environ.get('MEMCHACHED_PORT') or 11211
+
+cache = base.Client((host, port))
+
+@route('/test')
+def test():
+    logging.info('Hello World!')
+    return "Hello World!"
+
+
+@route('/')
+def alarm_map():
+    cached_data = cache.get('alarm_map')
+    logging.info('caching data get: %s' % cached_data)
+    response.content_type = 'application/json'
+    return cached_data or {'error': 'no data in cache'}
+
+
+logging.info('start viewer %s:%s' % (host, port))
+run(host='0.0.0.0', port=8080, debug=True)
