e29c2ba123af6bc9a92be130cd02d76c42b70ae5
v00g100skr <utmcrow@gmail.com>
2023-06-26 22:44:42 +0300

    покращення прошивки:  - підтримка буззера (Closes #38)
    покращення редактора:
     - налаштування буззера

--- src/firmware.h
+++ src/firmware.h
@@ -92,7 +92,18 @@ const char* apiKey = ""; //API погоди
 float minTemp = 5.0; // мінімальна температура у градусах Цельсія для налашутвання діапазону кольорів
 float maxTemp = 30.0; // максимальна температура у градусах Цельсія для налашутвання діапазону кольорів
 
+//Домашній регіон
 int stateId = 7;
+
+//Буззер
+bool enableBuzzer = true;
+bool enableBuzzerAtNight = false;
+int buzzerTone = 100;
+int buzzerDuration = 300;
+int buzzerCount = 3;
+int buzzerTempo = 100;
+int buzzerStartSound = 4;
+int buzzerEndSound = 3;
 
 //Налаштуванння повернення в режим тривог
 int statesIdsAlarmCheck[] PROGMEM = {
@@ -154,7 +165,7 @@ static String states[] PROGMEM = {
 };
 
 //Погодні IDS
-int statesIdsWeather[] PROGMEM =  {
+int statesIdsWeather[] PROGMEM = {
   690548,
   707471,
   691650,
@@ -218,6 +229,8 @@ static int flagColor[] PROGMEM {
 #define LED_TYPE        WS2812
 #define COLOR_ORDER     GRB
 
+#define BUZZER_PIN      16
+
 #define DISPLAY_WIDTH   128
 #define DISPLAY_HEIGHT  32
 // ======== КІНЕЦь НАЛАШТУВАННЯ =========
@@ -236,7 +249,6 @@ const int eepromMapModeAddress = 21;
 const int eepromDisplayModeAddress = 22;
 const int eepromModulationModeAddress = 23;
 
-//String baseURL = "https://map.vglskr.net.ua/alarm_map/statuses.json";
 String baseURL = "https://vadimklimenko.com/map/statuses.json";
 
 WiFiClient client;
@@ -307,6 +319,10 @@ static int ledColor[NUM_LEDS];
 
 bool isAlarm = false;
 bool isDaylightSaving = false;
+bool isBuzzerStart = true;
+bool isBuzzerEnd = true;
+bool buzzerSound = true;
+bool isDay;
 
 float currentTemp;
 String currentWeather;
@@ -331,6 +347,120 @@ const unsigned char trident_small [] PROGMEM = {
 	0x00, 0x07, 0xf0, 0x00, 0x00, 0x03, 0xe0, 0x00, 0x00, 0x01, 0xc0, 0x00, 0x00, 0x00, 0x80, 0x00
 };
 
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+#define REST      0
+
+int imperialMarch[] = {
+  NOTE_A4,4, NOTE_A4,4, NOTE_A4,4, NOTE_F4,-8, NOTE_C5,16,
+  NOTE_A4,4, NOTE_F4,-8, NOTE_C5,16, NOTE_A4, 2,
+};
+
+int ukrainianAnthem[] = {
+  NOTE_D5, 3, NOTE_D5, 8, NOTE_D5, 8, NOTE_C5, 8, NOTE_D5, 8, NOTE_DS5, 8, NOTE_F5, 3,
+  NOTE_DS5, 8, NOTE_D5, 4, NOTE_C5, 4,
+  NOTE_AS4, 4, NOTE_D5, 4, NOTE_A4, 4, NOTE_D5, 4, NOTE_G4, 2, NOTE_G4, 2,
+};
+
+int startrek[] = {
+  NOTE_D4, -8, NOTE_G4, 16, NOTE_C5, -4,
+  NOTE_B4, 8, NOTE_G4, -16, NOTE_E4, -16, NOTE_A4, -16,
+  NOTE_D5, 2,
+};
+
+int nokia[] = {
+  NOTE_E5, 16, NOTE_D5, 16, NOTE_FS4, 8, NOTE_GS4, 8,
+  NOTE_CS5, 16, NOTE_B4, 16, NOTE_D4, 8, NOTE_E4, 8,
+  NOTE_B4, 16, NOTE_A4, 16, NOTE_CS4, 8, NOTE_E4, 8,
+  NOTE_A4, 4,
+};
 
 
 void initHA() {
@@ -397,6 +527,7 @@ void initHA() {
     haUsedMemory.setUnitOfMeasurement("kB");
     haUsedMemory.setDeviceClass("data_size");
 
+    device.enableLastWill();
     mqtt.begin(brokerAddr,mqttPort,mqttUser,mqttPassword);
     Serial.println("mqtt connected");
   }
@@ -1031,7 +1162,7 @@ void autoBrightnessUpdate() {
       timeClient.update();
       int currentHour = timeClient.getHours();
       int currentBrightness = 0;
-      bool isDay = currentHour >= day && currentHour < night;
+      isDay = currentHour >= day && currentHour < night;
       //Serial.print("isDay: ");
       //Serial.print(isDay);
       currentBrightness = isDay ? dayBrightness : nightBrightness;
@@ -1161,8 +1292,6 @@ void alamsUpdate() {
           time_t currentTime = timeClient.getEpochTime();
           timeDifference = difftime(currentTime, oldTime);
           if (isDaylightSaving){timeDifference -= 14400;} else {timeDifference -= 10800;};
-          //Serial.print("timeDifference: ");
-          //Serial.println(timeDifference);
           isAlarm = true;
         }
         alarmsNowCount++;
@@ -1184,8 +1313,6 @@ void alamsUpdate() {
           time_t currentTime = timeClient.getEpochTime();
           timeDifference = difftime(currentTime, oldTime);
           if (isDaylightSaving){timeDifference -= 14400;} else {timeDifference -= 10800;};
-          ////Serial.print("timeDifference: ");
-          ////Serial.println(timeDifference);
           isAlarm = false;
         }
       }
@@ -1395,12 +1522,101 @@ void checkEEPROM() {
   }
 }
 
+
+void melody(int song[], int notes) {
+  Serial.println("melody start");
+  Serial.print("notes: ");
+  Serial.println(notes);
+  int wholenote = (60000 * 4) / buzzerTempo;
+  int divider = 0, noteDuration = 0;
+  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+    divider = song[thisNote + 1];
+    if (divider > 0) {
+      noteDuration = (wholenote) / divider;
+    } else if (divider < 0) {
+      noteDuration = (wholenote) / abs(divider);
+      noteDuration *= 1.5;
+    }
+    tone(BUZZER_PIN, song[thisNote], noteDuration*0.9);
+    delay(noteDuration);
+    noTone(BUZZER_PIN);
+    Serial.println(thisNote);
+  }
+  Serial.println("melody end");
+}
+
+void buzzer() {
+  for (int i = 0; i < buzzerCount; i++) {
+    tone(BUZZER_PIN, buzzerTone);
+    delay(buzzerDuration);
+    noTone(BUZZER_PIN);
+    delay(buzzerDuration);
+  }
+}
+
+void initBuzzer() {
+  if(enableBuzzer) {
+    pinMode(BUZZER_PIN, OUTPUT);
+    if(enableBuzzerAtNight){
+      melody(ukrainianAnthem, sizeof(ukrainianAnthem) / sizeof(ukrainianAnthem[0]) / 2 );
+    }
+  }
+}
+
+void buzzerUpdate() {
+  if(enableBuzzer) {
+    if (isAlarm && isBuzzerStart) {
+      if (isDay || (!isDay && enableBuzzerAtNight)) {
+        if (buzzerStartSound == 2){
+          buzzer();
+        }
+        if (buzzerStartSound == 3){
+          melody(ukrainianAnthem, sizeof(ukrainianAnthem) / sizeof(ukrainianAnthem[0]) / 2 );
+        }
+        if (buzzerStartSound == 4){
+          melody(imperialMarch, sizeof(imperialMarch) / sizeof(imperialMarch[0]) / 2 );
+        }
+        if (buzzerStartSound == 5){
+          melody(startrek, sizeof(startrek) / sizeof(startrek[0]) / 2 );
+        }
+        if (buzzerStartSound == 6){
+          melody(nokia, sizeof(nokia) / sizeof(nokia[0]) / 2 );
+        }
+      }
+      isBuzzerStart = false;
+      isBuzzerEnd = true;
+    }
+    if (!isAlarm && isBuzzerEnd) {
+      if (isDay || (!isDay && enableBuzzerAtNight)) {
+        if (buzzerEndSound == 2){
+          buzzer();
+        }
+        if (buzzerEndSound == 3){
+          melody(ukrainianAnthem, sizeof(ukrainianAnthem) / sizeof(ukrainianAnthem[0]) / 2 );
+        }
+        if (buzzerEndSound == 4){
+          melody(imperialMarch, sizeof(imperialMarch) / sizeof(imperialMarch[0]) / 2 );
+        }
+        if (buzzerEndSound == 5){
+          melody(startrek, sizeof(startrek) / sizeof(startrek[0]) / 2 );
+        }
+        if (buzzerEndSound == 6){
+          melody(nokia, sizeof(nokia) / sizeof(nokia[0]) / 2 );
+        }
+      }
+      isBuzzerStart = true;
+      isBuzzerEnd = false;
+    }
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   checkEEPROM();
   initFastLED();
   initDisplay();
   Flag(100);
+  initBuzzer();
   initWiFi();
   startText();
   initTime();
@@ -1427,6 +1643,7 @@ void loop() {
   weatherUpdate();
   displayInfo();
   mapInfo();
+  buzzerUpdate();
   uptime();
   delay(3000);
 }
