a2f5bbde93d3a5cf4af042946b6a9e5ba0694e97
v00g100skr <utmcrow@gmail.com>
2023-08-14 01:32:50 +0300

    покращення парсера:  - додана погода в asyncio

--- deploy/tcp_server/asyncio_cache.py
+++ deploy/tcp_server/asyncio_cache.py
@@ -3,6 +3,8 @@ import aiohttp
 import json
 import os
 
+from datetime import datetime, timezone
+
 map_data_url = os.environ.get('MAP_DATA') or 'http://10.2.0.40:8187/statuses_v1.json'
 tcp_port = os.environ.get('TCP_PORT') or 12345
 
@@ -39,6 +41,20 @@ regions = [
   "м. Київ",
 ]
 
+def get_local_time_formatted():
+    local_time = datetime.now(timezone.utc)
+    formatted_local_time = local_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+    return formatted_local_time
+
+
+def calculate_time_difference(timestamp1, timestamp2):
+    format_str = "%Y-%m-%dT%H:%M:%SZ"
+
+    time1 = datetime.strptime(timestamp1, format_str)
+    time2 = datetime.strptime(timestamp2, format_str)
+
+    time_difference = (time2 - time1).total_seconds()
+    return abs(time_difference)
 
 async def handle_client(reader, writer):
     global previous_data
@@ -79,11 +95,29 @@ async def parse_and_broadcast(server, clients):
                 new_data = await response.text()
                 parsed_data = json.loads(new_data)
 
-            tcp_data = []
+            local_time = get_local_time_formatted()
+            alerts = []
+            weather = []
             for region in regions:
-                tcp_data.append(1 if parsed_data['states'][region]['enabled'] else 0)
+                if parsed_data['states'][region]['enabled']:
+                    time_diff = calculate_time_difference(parsed_data['states'][region]['enabled_at'], local_time)
+                    if time_diff > 300:
+                        alert_mode = 1
+                    else:
+                        alert_mode = 3
+                else:
+                    time_diff = calculate_time_difference(parsed_data['states'][region]['disabled_at'], local_time)
+                    if time_diff > 300:
+                        alert_mode = 0
+                    else:
+                        alert_mode = 2
 
-            tcp_data = ":".join(map(str, tcp_data))
+                weather_temp = int(parsed_data['states'][region]['weather']['temp'])
+
+                alerts.append(str(alert_mode))
+                weather.append(str(weather_temp))
+
+            tcp_data = "%s:%s" % (",".join(alerts), ",".join(weather))
 
             if tcp_data != previous_data:
                 print("Data changed. Broadcasting to clients...")
@@ -95,12 +129,18 @@ async def parse_and_broadcast(server, clients):
         except Exception as e:
             print("Error:", e)
 
-        await asyncio.sleep(5)
+        await asyncio.sleep(2)
 
 
 async def log_clients_periodically(clients):
     while True:
         print(f"Number of connected clients: {len(clients)}")
+        await asyncio.sleep(10)
+
+
+async def show_cache(clients):
+    while True:
+        print(f"Cached data: {previous_data}")
         await asyncio.sleep(10)
 
 
@@ -112,6 +152,7 @@ if __name__ == "__main__":
 
     asyncio.ensure_future(parse_and_broadcast(server, clients))
     asyncio.ensure_future(log_clients_periodically(clients))
+    asyncio.ensure_future(show_cache(clients))
 
     try:
         loop.run_forever()
