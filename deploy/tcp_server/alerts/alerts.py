51812ed81d180d19ce7bce77f791a56091443129
v00g100skr <utmcrow@gmail.com>
2023-12-06 02:06:57 +0200

    тестова версія сервера

---
+++ deploy/tcp_server/alerts/alerts.py
@@ -0,0 +1,177 @@
+import json
+import os
+import asyncio
+import aiohttp
+import logging
+from aiomcache import Client
+
+from datetime import datetime
+
+from copy import copy
+
+version = 2
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+alarm_url = "https://api.ukrainealarm.com/api/v3/alerts"
+region_url = "https://api.ukrainealarm.com/api/v3/regions"
+
+alert_token = os.environ.get('ALERT_TOKEN') or 'token'
+memcached_host = os.environ.get('MEMCACHED_HOST') or 'localhost'
+
+alert_loop_time = int(os.environ.get('ALERT_PERIOD')) or 3
+
+# Authorization header
+headers = {
+    "Authorization": "%s" % alert_token
+}
+
+regions = {
+    "Закарпатська область": {"id": 1},
+    "Івано-Франківська область": {"id": 2},
+    "Тернопільська область": {"id": 3},
+    "Львівська область": {"id": 4},
+    "Волинська область": {"id": 5},
+    "Рівненська область": {"id": 6},
+    "Житомирська область": {"id": 7},
+    "Київська область": {"id": 8},
+    "Чернігівська область": {"id": 9},
+    "Сумська область": {"id": 10},
+    "Харківська область": {"id": 11},
+    "Луганська область": {"id": 12},
+    "Донецька область": {"id": 13},
+    "Запорізька область": {"id": 14},
+    "Херсонська область": {"id": 15},
+    "Автономна Республіка Крим": {"id": 16},
+    "Одеська область": {"id": 17},
+    "Миколаївська область": {"id": 18},
+    "Дніпропетровська область": {"id": 19},
+    "Полтавська область": {"id": 20},
+    "Черкаська область": {"id": 21},
+    "Кіровоградська область": {"id": 22},
+    "Вінницька область": {"id": 23},
+    "Хмельницька область": {"id": 24},
+    "Чернівецька область": {"id": 25},
+    "м. Київ": {"id": 26},
+}
+
+
+def calculate_time_difference(timestamp1, timestamp2):
+    format_str = "%Y-%m-%dT%H:%M:%SZ"
+
+    time1 = datetime.strptime(timestamp1, format_str)
+    time2 = datetime.strptime(timestamp2, format_str)
+
+    time_difference = (time2 - time1).total_seconds()
+    return abs(time_difference)
+
+
+async def alarm_data(mc):
+    try:
+        await asyncio.sleep(alert_loop_time)
+        alerts_cached = await mc.get(b"alerts")
+
+        if alerts_cached:
+            alerts_cached_data = json.loads(alerts_cached.decode('utf-8'))
+        else:
+            alerts_cached_data = {}
+
+        empty_data = {
+            'alertnow': False,
+            'changed': None,
+        }
+
+        current_datetime = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+        if alerts_cached_data.get('info', {}).get('is_started', False) is False \
+                or alerts_cached_data.get('version', 0) != version:
+            logging.debug("fill empty fields")
+            alerts_cached_data = {
+                "version": version,
+                "states": {},
+                "info": {
+                    "last_update": None,
+                    "is_started": False
+                }
+            }
+
+            logging.debug("fill start data")
+            async with aiohttp.ClientSession() as session:
+                response = await session.get(region_url, headers=headers)  # Replace with your URL
+                new_data = await response.text()
+                data = json.loads(new_data)
+            for item in data['states']:
+                region_name = item["regionName"]
+                alerts_cached_data["states"][region_name] = copy(empty_data)
+
+                region_alert_url = "%s/%s" % (alarm_url, item['regionId'])
+                async with aiohttp.ClientSession() as session:
+                    response = await session.get(region_alert_url, headers=headers)  # Replace with your URL
+                    new_data = await response.text()
+                    region_data = json.loads(new_data)[0]
+                alerts_cached_data["states"][region_name]["changed"] = region_data["lastUpdate"]
+
+        logging.debug("get data")
+        async with aiohttp.ClientSession() as session:
+            response = await session.get(alarm_url, headers=headers)  # Replace with your URL
+            new_data = await response.text()
+            data = json.loads(new_data)
+
+        logging.debug("parse activeAlerts")
+        alert_region_names = []
+        for item in data:
+            for alert in item["activeAlerts"]:
+                if (alert["regionId"] == item["regionId"]
+                        and alert["regionType"] == "State"
+                        and alert["type"] == "AIR"):
+                    region_name = item["regionName"]
+                    region_data = alerts_cached_data['states'].get(region_name, empty_data)
+                    alert_region_names.append(region_name)
+                    region_data["alertnow"] = True
+                    region_data["changed"] = alert['lastUpdate']
+                    alerts_cached_data["states"][region_name] = region_data
+
+        logging.debug("parse states")
+        for region_name, data in regions.items():
+            if region_name not in alert_region_names and alerts_cached_data['states'][region_name]['alertnow'] is True:
+                alerts_cached_data['states'][region_name]['alertnow'] = False
+                alerts_cached_data['states'][region_name]['changed'] = current_datetime
+
+        alerts_cached_data['info']['is_started'] = True
+        alerts_cached_data['info']['last_update'] = current_datetime
+        logging.debug("store alerts data: %s" % current_datetime)
+        await mc.set(b"alerts", json.dumps(alerts_cached_data).encode('utf-8'))
+        logging.debug("alerts data stored")
+
+    except Exception as e:
+        logging.error(f"Error fetching data: {str(e)}")
+        raise
+
+
+async def main():
+    mc = Client(memcached_host, 11211)  # Connect to your Memcache server
+    while True:
+        try:
+            logging.debug("Task started")
+            await alarm_data(mc)
+
+        except asyncio.CancelledError:
+            logging.info("Task canceled. Shutting down...")
+            await mc.close()
+            break
+
+        except Exception as e:
+            logging.error(f"Caught an exception: {e}")
+
+        finally:
+            logging.debug("Task completed")
+            pass
+
+
+if __name__ == "__main__":
+    try:
+        logging.debug("Start")
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logging.debug("KeyboardInterrupt")
+        pass
--- deploy/tcp_server/redeploy_weather.sh
+++ deploy/tcp_server/redeploy_weather.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 # Default values
-ALERT_TOKEN=""
+WEATHER_TOKEN=""
 MEMCACHED_HOST=""
+WEATHER_PERIOD=600
 
 # Check for arguments
 while [[ $# -gt 0 ]]; do
     case "$1" in
-        -a|--alert-token)
-            ALERT_TOKEN="$2"
+        -w|--alert-token)
+            WEATHER_TOKEN="$2"
             shift 2
             ;;
         -m|--memcached-host)
             MEMCACHED_HOST="$2"
+            shift 2
+            ;;
+        -m|--weather-period)
+            WEATHER_PERIOD="$2"
             shift 2
             ;;
         *)
@@ -22,10 +27,11 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-echo "ALERTS"
+echo "WEATHER"
 
-echo "ALERT_TOKEN: $ALERT_TOKEN"
+echo "WEATHER_TOKEN: $WEATHER_TOKEN"
 echo "MEMCACHED_HOST: $MEMCACHED_HOST"
+echo "WEATHER_PERIOD: $WEATHER_PERIOD"
 
 
 # Updating the Git repo
@@ -35,20 +41,20 @@ git pull
 
 # Moving to the deployment directory
 echo "Moving to deployment directory..."
-cd /alerts
+cd /weather
 
 # Building Docker image
 echo "Building Docker image..."
-docker build -t alerts -f Dockerfile .
+docker build -t weather -f Dockerfile .
 
 # Stopping and removing the old container (if exists)
 echo "Stopping and removing old container..."
-docker stop alerts || true
-docker rm alerts || true
+docker stop weather || true
+docker rm weather || true
 
 # Deploying the new container
 echo "Deploying new container..."
-docker run --name alerts --restart unless-stopped -d --env ALERT_TOKEN="$ALERT_TOKEN" --env MEMCACHED_HOST="$MEMCACHED_HOST" alerts
+docker run --name weather --restart unless-stopped -d --env WEATHER_PERIOD="$WEATHER_PERIOD" --env WEATHER_TOKEN="$WEATHER_TOKEN" --env MEMCACHED_HOST="$MEMCACHED_HOST" weather
 
 echo "Container deployed successfully!"
 
