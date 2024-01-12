8ab7487b72caf7cc198560f4007c4ab9d6cbf297
v00g100skr <utmcrow@gmail.com>
2024-01-12 21:13:30 +0200

    added region data

--- deploy/web_server/async_server.py
+++ deploy/web_server/async_server.py
@@ -357,7 +357,7 @@ async def region_data_v1(request):
     iso_datetime_str = alerts_cached_data['states'][region]['changed']
     datetime_obj = datetime.fromisoformat(iso_datetime_str.replace("Z", "+00:00"))
     datetime_obj_utc = datetime_obj.replace(tzinfo=timezone.utc)
-    alerts_cached_data['states'][region]['changed'] = datetime_obj_utc.timestamp()
+    alerts_cached_data['states'][region]['changed'] = int(datetime_obj_utc.timestamp())
 
     return JSONResponse({
         'version': 1,
