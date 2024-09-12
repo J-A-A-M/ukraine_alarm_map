509ad1bb521650ff78017dd16356355a38740139
v00g100skr <utmcrow@gmail.com>
2024-09-12 23:19:06 +0300

    websockes dev

--- deploy/redeploy_websocket_server_dev.sh
+++ deploy/redeploy_websocket_server_dev.sh
@@ -6,6 +6,7 @@ WEBSOCKET_PORT=38447
 PING_INTERVAL=60
 ENVIRONMENT="DEV"
 RANDOM_MODE="False"
+TEST_MODE="False"
 MEMCACHE_FETCH_INTERVAL=1
 LOGGING="DEBUG"
 
@@ -36,6 +37,10 @@ while [[ $# -gt 0 ]]; do
             RANDOM_MODE="$2"
             shift 2
             ;;
+        -r|--test-mode)
+            TEST_MODE="$2"
+            shift 2
+            ;;
         -f|--fetch-interval)
             MEMCACHE_FETCH_INTERVAL="$2"
             shift 2
@@ -58,6 +63,7 @@ echo "WEBSOCKET_PORT: $WEBSOCKET_PORT"
 echo "PING_INTERVAL: $PING_INTERVAL"
 echo "ENVIRONMENT: $ENVIRONMENT"
 echo "RANDOM_MODE: $RANDOM_MODE"
+echo "TEST_MODE: $TEST_MODE"
 echo "MEMCACHE_FETCH_INTERVAL: $MEMCACHE_FETCH_INTERVAL"
 echo "LOGGING: $LOGGING"
 
@@ -82,7 +88,7 @@ docker rm map_websocket_server_dev || true
 
 # Deploying the new container
 echo "Deploying new container..."
-docker run --name map_websocket_server_dev --restart unless-stopped -d  -p "$WEBSOCKET_PORT":"$WEBSOCKET_PORT" --env WEBSOCKET_PORT="$WEBSOCKET_PORT" --env API_SECRET="$API_SECRET" --env MEASUREMENT_ID="$MEASUREMENT_ID" --env PING_INTERVAL="$PING_INTERVAL" --env MEMCACHED_HOST="$MEMCACHED_HOST" --env ENVIRONMENT="$ENVIRONMENT" --env RANDOM_MODE="$RANDOM_MODE" --env MEMCACHE_FETCH_INTERVAL="$MEMCACHE_FETCH_INTERVAL" --env LOGGING="$LOGGING" map_websocket_server_dev
+docker run --name map_websocket_server_dev --restart unless-stopped -d  -p "$WEBSOCKET_PORT":"$WEBSOCKET_PORT" --env WEBSOCKET_PORT="$WEBSOCKET_PORT" --env API_SECRET="$API_SECRET" --env MEASUREMENT_ID="$MEASUREMENT_ID" --env PING_INTERVAL="$PING_INTERVAL" --env MEMCACHED_HOST="$MEMCACHED_HOST" --env ENVIRONMENT="$ENVIRONMENT" --env RANDOM_MODE="$RANDOM_MODE" --env TEST_MODE="$TEST_MODE" --env MEMCACHE_FETCH_INTERVAL="$MEMCACHE_FETCH_INTERVAL" --env LOGGING="$LOGGING" map_websocket_server_dev
 
 echo "Container deployed successfully!"
 
