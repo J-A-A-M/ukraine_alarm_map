41af1c19cfaeeec6e9999bf0ac14dab4047c8f27
Yuriy Novostavskiy <yuriy@novostavskiy.kiev.ua>
2025-01-07 12:48:50 +0000

    resolve merge conflict

--- deploy/web_server/web_server.py
+++ deploy/web_server/web_server.py
@@ -155,8 +155,8 @@ async def main(request):
                     <p>Корисні посилання:</p>
                     <ul>
                         <li><a href="https://github.com/J-A-A-M/ukraine_alarm_map">ukraine_alarm_map (github-репозіторій)</a></li>
-                        <li><a href="https://t.me/jaam_project">Канал з новинами</a> - підпишіться, будь-ласка :-) </li> 
-                        <li><a href="https://t.me/jaam_discussions">Група для обговорень</a></li>                             
+                        <li><a href="https://t.me/jaam_project">Канал з новинами</a> - підпишіться, будь-ласка :-) </li>
+                        <li><a href="https://t.me/jaam_discussions">Група для обговорень</a></li>
                     </ul>
                 </div>
                 <div class='col-md-6 offset-md-3'>
@@ -165,17 +165,17 @@ async def main(request):
                         <li>Тривоги: [<a href="/alerts_statuses_v1.json">v1</a>], [<a href="/alerts_statuses_v2.json">v2</a>], [<a href="/alerts_statuses_v3.json">v3</a>]</li>
                         <li>Погода: [<a href="/weather_statuses_v1.json">v1</a>]</li>
                         <li>Вибухи: (інформація зі ЗМІ) [<a href="/explosives_statuses_v1.json">v1</a>], [<a href="/explosives_statuses_v2.json">v2</a>], [<a href="/explosives_statuses_v3.json">v3</a>]</li>
-                        
+
                         <li><a href="/api_status.json">API healthcheck</a></li>
                     </ul>
                 </div>
-                
+
                 <div class='col-md-6 offset-md-3'>
                     <p>Джерела даних:</p>
                     <ul>
                         <li><a href="https://app.etryvoga.com/">app.etryvoga.com (дані по вибухам зі ЗМІ)</a></li>
                         <li><a href="https://www.ukrainealarm.com/">ukrainealarm.com (офіційне API тривог)</a></li>
-                        <li><a href="https://openweathermap.org/api">openweathermap.org (погода)</a></li>                         
+                        <li><a href="https://openweathermap.org/api">openweathermap.org (погода)</a></li>
                     </ul>
                 </div>
                 <div class='col-md-6 offset-md-3'>
@@ -543,7 +543,7 @@ async def stats(request):
 
         return JSONResponse(
             {
-                "map": {f'{data.get("ip")}_{data.get("port")}': f"1" for data in map_clients_data},
+                "map": {f'{data.get("port")}': f"1" for data in map_clients_data},
                 "google": map_clients_data,
                 "api": {ip: f"{int(time.time() - float(data[0]))} {data[1]}" for ip, data in api_clients.items()},
                 "img": {ip: f"{int(time.time() - float(data[0]))} {data[1]}" for ip, data in image_clients.items()},
