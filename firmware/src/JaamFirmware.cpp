dc06edf615a15e53fd907ae388e9e385f8b16360
Vladyslav Heneraliuk <vladyslav.heneraliuk@gmail.com>
2025-01-10 15:05:27 +0200

    loads marked.nim.js only when we need it

--- firmware/src/JaamFirmware.cpp
+++ firmware/src/JaamFirmware.cpp
@@ -2042,7 +2042,11 @@ void addFooter(AsyncResponseStream* response) {
   response->println("<script src='https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js' integrity='sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj' crossorigin='anonymous'></script>");
   response->println("<script src='https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js' integrity='sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct' crossorigin='anonymous'></script>");
   response->println("<script src='https://cdn.jsdelivr.net/npm/js-cookie@3.0.5/dist/js.cookie.min.js'></script>");
-  response->println("<script src='https://cdn.jsdelivr.net/npm/marked/marked.min.js'></script>");
+#if FW_UPDATE_ENABLED
+  if (fwUpdateAvailable) {
+    response->println("<script src='https://cdn.jsdelivr.net/npm/marked/marked.min.js'></script>");
+  }
+#endif
   response->print("<script src='https://");
   response->print(settings.serverhost);
   response->println("/static/jaam_v1.js'></script>");
