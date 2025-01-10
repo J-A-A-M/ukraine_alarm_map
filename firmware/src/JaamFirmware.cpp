f0b99eb7670122463732a745d8cee8932b3e0f62
Foroxon <Foroxon@users.noreply.github.com>
2025-01-10 21:38:44 +0000

    Beta 4.2-b89

--- firmware/src/JaamFirmware.cpp
+++ firmware/src/JaamFirmware.cpp
@@ -32,7 +32,7 @@
 #endif
 #include <esp_task_wdt.h>
 
-const PROGMEM char* VERSION = "4.2-b88";
+const PROGMEM char* VERSION = "4.2-b89";
 
 struct Settings {
   const char*   apssid                 = "JAAM";
