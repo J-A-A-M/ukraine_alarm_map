4e9392c60b1ed315caccc95f0460e2a517a0368d
v00g100skr <808264+v00g100skr@users.noreply.github.com>
2025-01-17 21:40:14 +0000

    Beta 4.2-b92

--- firmware/src/JaamFirmware.cpp
+++ firmware/src/JaamFirmware.cpp
@@ -32,7 +32,7 @@
 #endif
 #include <esp_task_wdt.h>
 
-const PROGMEM char* VERSION = "4.2-b91";
+const PROGMEM char* VERSION = "4.2-b92";
 
 struct Settings {
   const char*   apssid                 = "JAAM";
