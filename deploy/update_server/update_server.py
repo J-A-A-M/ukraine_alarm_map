56d217185812722c0c94d3135388807bb007c435
Vladyslav Heneraliuk <vladyslav.heneraliuk@gmail.com>
2025-02-11 12:54:13 +0200

    allows forwarded ips

--- deploy/update_server/update_server.py
+++ deploy/update_server/update_server.py
@@ -245,4 +245,4 @@ app = Starlette(
 
 if __name__ == "__main__":
     asyncio.run(update_cache())
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port, proxy_headers=True, forwarded_allow_ips=["*"])
--- deploy/web_server/web_server.py
+++ deploy/web_server/web_server.py
@@ -649,4 +649,4 @@ app = Starlette(
 )
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port, proxy_headers=True, forwarded_allow_ips=["*"])
