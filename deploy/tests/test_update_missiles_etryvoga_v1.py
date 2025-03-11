20702dd23b1ed32ee11a8e4ebcacaa0bee0ddba9
v00g100skr <utmcrow@gmail.com>
2025-03-11 18:39:56 +0200

    refactor updater.py: notifications shows only if no states, states shows only if alerts exists update tests

--- deploy/tests/test_update_missiles_etryvoga_v1.py
+++ deploy/tests/test_update_missiles_etryvoga_v1.py
@@ -14,9 +14,7 @@ unix 1736935200 - 2025-01-15T10:00:00Z
 
 @pytest.mark.asyncio
 @patch("updater.updater.update_period", new=0)
-@patch("updater.updater.get_cache_data", new_callable=AsyncMock)
-@patch("updater.updater.get_etryvoga", new_callable=AsyncMock)
-async def test_1(mock_get_etryvoga, mock_get_cache_data):
+async def test_1():
     """
     нема даних для вебсокета в мемкеш
     зберігання першої тривоги
@@ -25,27 +23,33 @@ async def test_1(mock_get_etryvoga, mock_get_cache_data):
     mock_mc = AsyncMock(spec=Client)
     mock_mc.set.return_value = True
 
-    mock_get_etryvoga.return_value = {
-        "version": 1,
-        "states": {"11": {"lastUpdate": "2022-02-24T03:40:00Z"}, "13": {"lastUpdate": "2025-01-15T10:00:00Z"}},
-        "info": {
-            "last_update": "2025-01-26T19:18:55Z",
-            "last_id": "239a016a03c583633424afb5d418051b0a33a59374d0884912f8062336c09a93",
-        },
-    }
-    mock_get_cache_data.return_value = []
+    def mock_get_cache_data_side_effect(mc, key, default=None):
+        mock_responses = {
+            b"missiles_etryvoga": {
+                "version": 1,
+                "states": {"11": {"lastUpdate": "2022-02-24T03:40:00Z"}, "13": {"lastUpdate": "2025-01-15T10:00:00Z"}},
+                "info": {
+                    "last_update": "2025-01-26T19:18:55Z",
+                    "last_id": "239a016a03c583633424afb5d418051b0a33a59374d0884912f8062336c09a93",
+                },
+            }
+        }
+        return mock_responses.get(key, default)
 
-    await update_missiles_etryvoga_v1(mock_mc, run_once=True)
+    mock_get_cache_data = AsyncMock(side_effect=mock_get_cache_data_side_effect)
 
-    expected_result = [1645674000, 1736935200, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    mock_mc.set.assert_awaited_with(b"missiles_websocket_v1", json.dumps(expected_result).encode("utf-8"))
+    with (patch("updater.updater.get_cache_data", mock_get_cache_data),):
+        await update_missiles_etryvoga_v1(mock_mc, run_once=True)
+
+        expected_result = [1645674000] * 26
+        expected_result[1] = 1736935200
+
+        mock_mc.set.assert_awaited_with(b"missiles_websocket_v1", json.dumps(expected_result).encode("utf-8"))
 
 
 @pytest.mark.asyncio
 @patch("updater.updater.update_period", new=0)
-@patch("updater.updater.get_cache_data", new_callable=AsyncMock)
-@patch("updater.updater.get_etryvoga", new_callable=AsyncMock)
-async def test_2(mock_get_etryvoga, mock_get_cache_data):
+async def test_2():
     """
     є дані в memcache
     апдейт часу першої тривоги
@@ -54,44 +58,28 @@ async def test_2(mock_get_etryvoga, mock_get_cache_data):
     mock_mc = AsyncMock(spec=Client)
     mock_mc.set.return_value = True
 
-    mock_get_etryvoga.return_value = {
-        "version": 1,
-        "states": {"11": {"lastUpdate": "2025-01-15T10:00:00Z"}},
-        "info": {
-            "last_update": "2025-01-26T19:18:55Z",
-            "last_id": "239a016a03c583633424afb5d418051b0a33a59374d0884912f8062336c09a93",
-        },
-    }
-    mock_get_cache_data.return_value = [
-        1645674000,
-        1736935200,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-        0,
-    ]
+    def mock_get_cache_data_side_effect(mc, key, default=None):
+        mock_responses = {
+            b"missiles_etryvoga": {
+                "version": 1,
+                "states": {"11": {"lastUpdate": "2025-02-24T03:40:00Z"}},
+                "info": {
+                    "last_update": "2025-02-26T19:18:55Z",
+                    "last_id": "239a016a03c583633424afb5d418051b0a33a59374d0884912f8062336c09a93",
+                },
+            },
+            b"missiles_websocket_v1": [1736935200] * 26,
+            b"missiles_websocket_v2": [[0, 1645674000]] * 26,
+        }
+        return mock_responses.get(key, default)
 
-    await update_missiles_etryvoga_v1(mock_mc, run_once=True)
+    mock_get_cache_data = AsyncMock(side_effect=mock_get_cache_data_side_effect)
 
-    expected_result = [1736935200, 1736935200, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    mock_mc.set.assert_awaited_with(b"missiles_websocket_v1", json.dumps(expected_result).encode("utf-8"))
+    with (patch("updater.updater.get_cache_data", mock_get_cache_data),):
+        await update_missiles_etryvoga_v1(mock_mc, run_once=True)
+
+        expected_result = [1736935200] * 26
+        expected_result[0] = 1740368400
+
+        mock_mc.set.assert_awaited_with(b"missiles_websocket_v1", json.dumps(expected_result).encode("utf-8"))
+
