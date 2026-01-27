9284b35fd74ed8b545d3653b2143a1573509cebd
v00g100skr <utmcrow@gmail.com>
2026-01-27 21:50:51 +0200

    Update deploy scripts to add G_TOKEN parameter and change proxy flag to -pr

--- deploy/redeploy_ukrenergo.sh
+++ deploy/redeploy_ukrenergo.sh
@@ -39,7 +39,7 @@ while [[ $# -gt 0 ]]; do
             UKRENERGO_SOURCE_URL="$2"
             shift 2
             ;;
-        -p|--proxies)
+        -pr|--proxies)
             PROXIES="$2"
             shift 2
             ;;
