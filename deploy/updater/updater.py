0982073b69b4fde0a0dc312afdadda456280d38f
v00g100skr <utmcrow@gmail.com>
2026-05-02 23:54:00 +0300

    feat: Update WebSocket channel configuration for etryvoga alerts to use v2 endpoints

--- deploy/updater/updater.py
+++ deploy/updater/updater.py
@@ -434,19 +434,13 @@ async def update_websocket_v1_etryvoga(redis_client, run_once=False):
         "alerts:etryvoga:drones:updated": (
             "alerts:etryvoga:drones:data",
             "websocket:v1:legacy:drones",
-            None,
+            "websocket:v2:legacy:drones",
         ),
         "alerts:etryvoga:missiles:updated": (
             "alerts:etryvoga:missiles:data",
             "websocket:v1:legacy:missiles",
-            None,
+            "websocket:v2:legacy:missiles",
         ),
-        "alerts:etryvoga:explosions:updated": (
-            "alerts:etryvoga:explosions:data",
-            "websocket:v1:legacy:explosions",
-            None,
-        ),
-        "alerts:etryvoga:kabs:updated": ("alerts:etryvoga:kabs:data", "websocket:v1:legacy:kabs", None),
     }
 
     channels = list(channel_config.keys())
